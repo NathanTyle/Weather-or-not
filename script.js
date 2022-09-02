e58581e9c1906206690053e7a39407e50f1bfd60
Tyler Charro <tylercharro@gmail.com>
2022-09-02 16:09:32 -0500

    added fetch function based on city

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 let weather = {
     "apiKey": "93db155a8ef4aa952a0f37bd12123832",
+    fetchWeather: function (city) {
+      fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" 
+      + city 
+      + "&units=imperial&appid=" 
+      + this.apiKey
+      )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+    },
+};
