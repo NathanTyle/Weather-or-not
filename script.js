5683b82b4cd2d972724d7ca1e44d731b9933c08b
Tyler Charro <tylercharro@gmail.com>
2022-09-02 16:13:04 -0500

    added display functionality and search button functionality

--- script.js
+++ script.js
@@ -10,4 +10,26 @@ let weather = {
       .then((response) => response.json())
       .then((data) => this.displayWeather(data));
     },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon,description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed)
+        document.querySelector(".city").innerText = "Weather in" + name;
+        document.querySelector(".icon").src = 
+        "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "mph";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
 };
+
+document.querySelector(".search button")
+.addEventListener("click", function () {
+    weather.search();
+});
